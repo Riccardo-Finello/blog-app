3849c56a949b2ad4ed6155f3f4e24285a97d96ce
Riccardo-Finello <74531640+Riccardo-Finello@users.noreply.github.com>
2023-02-23 13:29:15 +0100

    regAndLog

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,18 +11,29 @@
             </div>
         </main>
       <div style="margin-left:35%; width:30%">
-      <form id="loginForm" method="POST" action="{{route("doLogin")}}">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+        @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                     @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                     @endforeach
+                    </ul>
+                </div>
+            @endif
+        <form id="loginForm" method="POST" action="{{route("doLogin")}}">
+          @csrf
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
         @include('partials.footer')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,27 +10,39 @@
               </div>
             </div>
         </main>
-      <form id="registerForm" method="POST" action="{{route("doRegister")}}">
-        <div class="form-group">
-            <label for="inputName">Name</label>
-            <input type="text" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter your name">
-        
+        <div style="margin-left:35%; width:30%">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                     @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                     @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form id="registerForm" method="POST" action="{{route("doRegister")}}">
+                @csrf
+                <div class="form-group">
+                    <label for="inputName">Name</label>
+                    <input type="text" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Enter your name" name="name">
+                
+                </div>
+                <div class="form-group">
+                <label for="inputMail">Email address</label>
+                <input type="email" class="form-control" id="inputMail" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password">
+                </div>
+                <div class="form-group">
+                <label for="inputPasswordRepeat">Password</label>
+                <input type="password" class="form-control" id="inputPasswordRepeat" placeholder="Repeat Password" name="confirmPassword">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-          <label for="inputMail">Email address</label>
-          <input type="email" class="form-control" id="inputMail" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="inputPassword">Password</label>
-          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-        </div>
-        <div class="form-group">
-          <label for="inputPasswordRepeat">Password</label>
-          <input type="password" class="form-control" id="inputPasswordRepeat" placeholder="Repeat Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
         @include('partials.footer')
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
